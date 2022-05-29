dbf82fd1112ca3ad8699d7fc5bd1527304760776
Yash <bajajy82@gmail.com>
2022-05-29 20:36:57 +0530

    Added Twitter Authentication using nextAuth

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -10,9 +10,11 @@ import {
   HomeIcon
 } from '@heroicons/react/outline'
 import SideBarRow from './SideBarRow'
+import { signIn, signOut, useSession } from 'next-auth/react'
 
 
 function SideBar() {
+  const {data:session} = useSession();
   return (
     <div className='flex flex-col col-span-2 items-center px-4 md:items-start' >
       <img className='m-3 h-10 w-10' alt='twitter' src="https://links.papareact.com/drq" />
@@ -22,7 +24,7 @@ function SideBar() {
       <SideBarRow title="Messages" Icon={MailIcon} />
       <SideBarRow title="Bookmarks" Icon={BookmarkIcon} />
       <SideBarRow title="Lists" Icon={CollectionIcon} />
-      <SideBarRow title="Sign In" Icon={UserIcon} />
+      <SideBarRow onClick={session? signOut:signIn}  title={session? "Sign Out":"Sign In"} Icon={UserIcon} />
       <SideBarRow title="More" Icon={DotsCircleHorizontalIcon} />
     
     </div>
--- components/SideBarRow.tsx
+++ components/SideBarRow.tsx
@@ -3,11 +3,12 @@ import React, { SVGProps } from 'react'
 interface Props {
   Icon: (props: SVGProps<SVGSVGElement>) => JSX.Element
   title: string
+  onClick?: ()=>{}
 }
 
-function SideBarRow({ Icon, title }: Props) {
+function SideBarRow({ Icon, title,onClick }: Props) {
   return (
-    <div className=' flex items-center rounded-full space-x-2 max-w-fit px-4 py-3 hover:bg-gray-100 cursor-pointer transition-all duration-200 group ' >
+    <div onClick={()=>onClick?.()} className=' flex items-center rounded-full space-x-2 max-w-fit px-4 py-3 hover:bg-gray-100 cursor-pointer transition-all duration-200 group ' >
       <Icon className='h-6 w-6' />
       <p className='hidden md:inline-flex group-hover:text-twitter text-base font-light lg:text-xl' >{title}</p>
     </div>
